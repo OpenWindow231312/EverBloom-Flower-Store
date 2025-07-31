fcfc3f5891990f295888025e398e460795f84616
OpenWindow231312 <231312@virtualwindow.co.za>
2025-07-31 22:03:56 +0200

    Navbar
    - Added links, added styles

--- frontend/everbloom/src/components/Navbar.js
+++ frontend/everbloom/src/components/Navbar.js
@@ -1,10 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { FaUser, FaHeart } from "react-icons/fa";
+import { ReactComponent as EverBloomLogo } from "../assets/EverBloomLogoLight.svg"; // <-- Updated Light Logo!
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <EverBloomLogo className="logo-icon" />
+        {/* <span className="brand-text">EverBloom</span> */}
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <NavLink to="/" className="nav-link" end>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className="nav-link">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/buy" className="nav-link">
+            Buy
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/dashboard" className="nav-link">
+            Dashboard
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/wishlist"
+            className="nav-icon-link"
+            aria-label="Wishlist"
+          >
+            <FaHeart />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/user" className="nav-icon-link" aria-label="Profile">
+            <FaUser />
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
